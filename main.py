be263153728ca0f5afd763142afba1193bf533c9
TheBrovo <rsj1134@pm.me>
2026-04-18 21:24:55 -0600

    Set 60 FPS limit, wrote main game loop, add hitbox code

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +18,9 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
