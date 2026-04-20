fa3839d0a1f869f210bf798ef43c79b2ec562512
TheBrovo <rsj1134@pm.me>
2026-04-19 19:57:48 -0600

    added asteroidfield and spawning and moving mechanics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroidfield import AsteroidField
+from asteroids import Asteroid
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -12,11 +14,19 @@ def main():
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -24,7 +34,7 @@ def main():
         screen.fill("black")
         updatable.update(dt)
         for draw in drawable:
-            draw.update(dt)
+            draw.draw(screen)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
